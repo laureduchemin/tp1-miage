2935d545c7e1ca6e44ad4264c6b1e8af66ac9b52
Laure Duchemin <duchemin_laure@yahoo.fr>
2018-03-30 15:31:03 +0200

    exo 1-2

--- ex1-ini/mailreader/src/main/java/com/acme/mailreader/presentation/InterpreteurLigneCommande.java
+++ ex1-ini/mailreader/src/main/java/com/acme/mailreader/presentation/InterpreteurLigneCommande.java
@@ -1,5 +1,14 @@
 package com.acme.mailreader.presentation;
 
+import com.acme.mailreader.domaine.Mail;
+import com.acme.mailreader.service.MailService;
+
 public class InterpreteurLigneCommande {
+	
+	public void nouveauMail(String[] args){
+		Mail mail = new Mail.Builder(args[1]).build();
+		MailService mailService = new MailService();
+		mailService.envoyerMail(mail);
+	}
 
 }
